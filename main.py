81ad48226db429324c3dc22327f0d691772ea2b2
nuru-d <nurud538@gmail.com>
2024-06-10 03:15:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ send_txn = algorand.send.asset_create(
     AssetCreateParams(
         sender=creator.address,
         total=2000,
-        asset_name="PAINLESS",
-        unit_name="MJD",
+        asset_name="NURU",
+        unit_name="EEN",
         manager=creator.address,
         clawback=creator.address,
         freeze=creator.address
@@ -94,7 +94,7 @@ algorand.send.asset_transfer(
         sender=creator.address,
         receiver=creator.address,
         asset_id=asset_id,
-        amount=15,
+        amount=5,
         clawback_target=receiver_acct.address
     )
 )
